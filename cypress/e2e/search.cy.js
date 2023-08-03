e3a7eab6f00dec1b2c881a324cf17442dec1bdb7
Alec Livaditis <aleclivaditis@gmail.com>
2023-08-03 12:05:46 -0400

    add sad path for search test

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -2,11 +2,13 @@ describe('search spec', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://www.googleapis.com/books/v1/volumes?q=%22terraformers%22+subject:%22fiction_science_fiction%22&startIndex=0&printType=books',
       {fixture: 'search.json'}).as('getBooks')
+    cy.intercept('GET', 'https://www.googleapis.com/books/v1/volumes?q=%22asewaewaerweaewaewaewaewaewa%22+subject:%22fiction_science_fiction%22&startIndex=0&printType=books',
+      {fixture: 'noresults.json'}).as('noresults')
+    cy.visit('http://localhost:3000/')
   })
   
   it('as a user I should be able to type into the search field, submit, and see a list of results', () => {
-    cy.visit('http://localhost:3000/')
-      .get('.search--input').type('terraformers')
+    cy.get('.search--input').type('terraformers')
       .get('.search--button').click()
       .get('.results-container').find('.card').should('have.length', '9')
       .get('.card').first().contains('The Terraformers')
@@ -17,5 +19,12 @@ describe('search spec', () => {
       .get('.card').last().contains('2021')
       .get('.card').first().click()
       .url().should('include', '/search/terraformers/0')
+  })
+
+  it('as a user I should see a message telling me if there are no results', () => {
+    cy.get('.search--input').type('asewaewaerweaewaewaewaewaewa')
+      .get('.search--button').click()
+      .get('.no-results').contains('No results for asewaewaerweaewaewaewaewaewa')
+
   })
 })
