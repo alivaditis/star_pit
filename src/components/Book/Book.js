1dfc193d5e79ff63bfe5f1ca1be078c27caba51a
Alec Livaditis <aleclivaditis@gmail.com>
2023-08-04 12:17:17 -0400

    style

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -30,20 +30,23 @@ function Book ({myBooks, addRemove, handleApiError}) {
   <>
     {isLoading ? 
       <p className='loading'>loading...</p> 
-      : <div className='book'>
+      : <div className='book-view'>
+        <div className='book'>
           <div className='book-container'>
             <img className='book-image' src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : ''} onError={onImageError}></img>
             <div className='book-info'>
               <p className='book-title'>{book.volumeInfo.title}</p>
               <p className='book-authors'>{book.volumeInfo.authors.join(', ')}</p>
-              <p>{book.volumeInfo.publisher}</p>
-              <p>{book.volumeInfo.publishedDate ? formatDate(book.volumeInfo.publishedDate) : ''}</p>
+              <p className='secondary-detail'>{book.volumeInfo.publisher}</p>
+              <p className='secondary-detail'>{book.volumeInfo.publishedDate ? formatDate(book.volumeInfo.publishedDate) : ''}</p>
+              <p className='secondary-detail'>{book.volumeInfo.pageCount} pages</p>
               <div className='book-button'>
                   <ButtonToRead className='book-button' myBooks={myBooks} addRemove={addRemove} book={cleanupSingleBook(book)}/>
               </div>
             </div>
           </div>
-          <div className='book-description'>{removeTags(book.volumeInfo.description)}</div>
+        </div>
+        <div className='book-description'>{removeTags(book.volumeInfo.description)}</div>
         </div>}
   </>
   )
